fc7b165e32191d2fb1d70784dc8eb5e6291c195b
datDat <32026170+cuocchie@users.noreply.github.com>
2025-05-26 22:11:33 +0700

    Configure build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nana-tracking/'
+  base: '/nana-tracking/',
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
 })
